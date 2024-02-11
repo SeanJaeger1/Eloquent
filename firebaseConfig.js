a4f97d85df1b894e2315d88ac08eb5e848387c02
Sean Jaeger <seanljaeger@gmail.com>
2024-02-11 10:01:16 +0000

    Upgraded packages

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "@firebase/app"
 import { getAuth } from "@firebase/auth"
 import { getFunctions } from "@firebase/functions"
-import {getFirestore} from "@firebase/firestore"
+import { getFirestore } from "@firebase/firestore"
 
 import Constants from "expo-constants"
 
-const firebaseConfig = Constants.manifest.extra.firebaseConfig
+const firebaseConfig = Constants.expoConfig.extra.firebaseConfig
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
-const functions = getFunctions(app, 'europe-west1')
-const db = getFirestore(app);
+const functions = getFunctions(app, "europe-west1")
+const db = getFirestore(app)
 
 export { auth, functions, db }
