f0e47451d8fff26d75f8855fe87a92cd2feabcf6
Sean Jaeger <seanljaeger@gmail.com>
2024-02-26 09:48:25 +0000

    Fixed progress meter

--- components/ProgressMeter.jsx
+++ components/ProgressMeter.jsx
@@ -1,33 +1,40 @@
-import { View } from "react-native"
-
+import React from "react"
+import { View, StyleSheet } from "react-native" // Corrected import
 import palette from "../palette"
 
 const DotsComponent = ({ value }) => {
   const dots = Array(5).fill(null)
   const filledDots = value > 0 && value <= 5 ? value : 0
 
-  const styles = StyleSheet.create({
-    container: {
-      display: "flex",
-      flexDirection: "row",
-    },
-    box: {
-      width: 8,
-      height: 8,
-      borderRadius: "50%",
-      backgroundColor:
-        index < filledDots ? palette.darkBlue : palette.lightBlue,
-      marginRight: 4,
-    },
-  })
-
   return (
     <View style={styles.container}>
       {dots.map((_, index) => (
-        <View key={index} style={styles.meter} />
+        <View
+          key={index}
+          style={[
+            styles.dot,
+            {
+              backgroundColor:
+                index < filledDots ? palette.darkBlue : palette.lightBlue,
+            },
+          ]}
+        />
       ))}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginRight: 4,
+  },
+})
 
 export default DotsComponent
