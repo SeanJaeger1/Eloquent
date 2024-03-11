d12df4b3ee78820326d5a4f774134899c09edb19
Sean Jaeger <seanljaeger@gmail.com>
2024-03-11 07:01:13 +0000

    Consolidated app.config.js and app.json files

--- app.config.js
+++ app.config.js
@@ -5,6 +5,29 @@ export default {
     name: "Eloquent",
     slug: "eloquent",
     version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/favicon.png",
+    },
     extra: {
       firebaseConfig: {
         apiKey: process.env.FIREBASE_API_KEY,
@@ -15,6 +38,10 @@ export default {
         appId: process.env.FIREBASE_APP_ID,
         measurementId: process.env.FIREBASE_MEASUREMENT_ID,
       },
+      eas: {
+        projectId: "9ef30b88-ee33-41c5-9039-11c413596d0a",
+      },
     },
+    owner: "seanjaeger123",
   },
 }
